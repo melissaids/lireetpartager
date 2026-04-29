b965ea25c32e3c81972c3db5524efbf9a424c948
Melissa Inacio Dos Santos <71281728+melissaids@users.noreply.github.com>
2026-04-29 14:39:41 +0200

    Code commenté
    Ajout de ligne pour commenter les fonctionnalitées

--- proposer.php
+++ proposer.php
@@ -1,16 +1,19 @@
 <?php
+// Initialise ou récupère la session utilisateur
 session_start();
 require_once 'config/db.php';
+// Change le nom de la page 
 $page_title = "Suggérer un livre";
 include 'models/header.php';
-
+// Stockage des messages d'erreur ou de reussite
 $msg = "";
-
+// Vérification si le formulaire a été soumis en POST & l'utilisateur est bien connecté
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION['user_id'])) {
+    // Récuperation & Suppression des données envoyées 
     $titre = trim($_POST['titre']);
     $auteur = trim($_POST['auteur']);
     $comm = trim($_POST['commentaire']);
-    
+    // Vérification si l'un des champs ce soit pas vide 
     if (!empty($titre) && !empty($auteur)) {
         $ins = $pdo->prepare("INSERT INTO suggestions (id_utilisateur, titre, auteur, commentaire) VALUES (?, ?, ?, ?)");
         $ins->execute([$_SESSION['user_id'], $titre, $auteur, $comm]);
